172caa3ec9155e3336952db3e758a8c24e548d1d
Jerry Nwosu <jerrynwosu007@gmail.com>
2020-11-01 04:24:45 +0100

    added code for the board structure

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-Js
-Js
+const board = document.querySelector(".board");
+
+const chessboard = (size = 8) => {
+  let board = "";
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += `<div class="dark"></div>`;
+      } else {
+        board += `<div class="light"></div>`;
+      }
+    }
+    board += "\n";
+  }
+  return board;
+};
+
+board.innerHTML = chessboard();
